f62a40f11bcc1809b28ab772f49db20fd556dcb3
JianfeiHua <jianfeihua@gmail.com>
2016-09-22 01:01:19 -0500

    Update con.js

--- conflict/con.js
+++ conflict/con.js
@@ -1,21 +1,4 @@
-/**
- * Transform the candidate object into as small a format as possible for caching (to preserve memory).
- * Normalize field names for the different possible candidate object formats that can be passed
- * @param {Object} candidate
- * @param {boolean=} opt_markRead If this is true, the candidate in the cache will be assumed to be read
- * @return {Object} The compressed version of the candidate
- * @private
- */
-indeed.dradis.controllers.CandidateDetailsController.prototype.normalizeCandidateListCandidate_ = function(candidate, opt_markRead) {
-    var isRead;
-    if (opt_markRead) {
-        isRead = true;
-    } else if (candidate.hasOwnProperty('read')) {
-        isRead = candidate['read'];
-    } else {
-        isRead = !candidate['unread'];
-    }
-    return [
+/
         candidate['encryptedCandidateId'] || candidate['candidateId'],//0
         candidate['name'],//1
         isRead,//2
